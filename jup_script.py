0cd3d37769935e25c90cb3b3c431f967cb7de513
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2021-12-11 14:37:48 -0500

    Create jup_script.py

---
+++ jup_script.py
@@ -0,0 +1,3 @@
+import pandas as pd
+player_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/playerStats.csv")
+advanced_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/AdvancedStats.csv")
