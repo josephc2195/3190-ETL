93d0f56601daf06e9bc1d49ef1d8c465bba59de7
josephc2195 <chicaj1221@gmail.com>
2021-12-11 18:10:07 -0500

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from pandas.io.parquet import to_parquet
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import plotly.express as px
 import os
@@ -9,10 +11,55 @@ import pandas as pd
 
 cwd = os.getcwd()
 
-playerStats = pd.read_csv(cwd + "/Datasets/playerStats.csv")
-adStats = pd.read_csv(cwd + "/Datasets/advancedStats.csv")
+player_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/playerStats.csv")
+advanced_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/AdvancedStats.csv")
+attendance = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/attendance.csv")
+stats_and_salaries = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/stats_and_salary.csv")
+
+
+teams = attendance.loc["0":, "TEAM"].values.tolist()
+att_percent = attendance.loc["0":, "PCT"].values.tolist()
+
 app = dash.Dash(__name__)
 server = app.server
+app.title = "Dashboard"
+topMenu = html.Header(role='banner', children=[
+        html.Div([
+                html.A("Home", href="/")
+        ])
+])
+
+app.layout = html.Div([
+        dcc.Location(id='url', refresh=False),
+        topMenu,
+        html.Div(id="content")
+])
+index_page = html.Div(children=[
+        html.H1("Which are the most popular teams?"),
+        html.Br(),
+        dcc.Graph(figure={
+                'data': [
+                        {'x': teams, 'y': att_percent, 'type': 'bar', 'name': 'name'},
+                ],
+                'layout': {
+                        'title': 'Percentage of Attendance per team'
+                }
+        }),
+
+        html.H2("Second Graph"),
+
+        
+
+])
+
+@app.callback(Output(component_id="content", component_property="children"),
+[Input(component_id='url', component_property="pathname")])
+def display_page(pathname):
+        if pathname =="/page-1":
+                return page_1_layout
+        else:
+                return index_page
+
 
 if __name__ == '__main__':
         app.run_server()
--- jup_script.py
+++ jup_script.py
@@ -1,3 +1,27 @@
 import pandas as pd
+from bokeh.io import show, output_notebook
+from bokeh.models import CategoricalColorMapper, ColumnDataSource, FactorRange
+from bokeh.plotting import figure, output_file
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
 player_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/playerStats.csv")
 advanced_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/AdvancedStats.csv")
+attendance = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/attendance.csv")
+stats_and_salaries = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/stats_and_salary.csv")
+
+teams = attendance.loc["0":, "TEAM"].values.tolist()
+att_percent = attendance.loc["0":, "PCT"].values.tolist()
+
+
+fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+counts = [5, 3, 4, 2, 4, 6]
+
+p = figure(x_range=fruits, plot_height=250, toolbar_location=None, title="Fruit Counts")
+p.vbar(x=fruits, top=counts, width=0.9)
+p.xgrid.grid_line_color = None
+p.y_range.start = 0
+output_notebook()
+show(p)
