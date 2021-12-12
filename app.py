9b69b04d9018b829daea350c8bc00fa0de82e12e
josephc2195 <chicaj1221@gmail.com>
2021-12-12 02:46:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,35 @@ import pandas as pd
 
 cwd = os.getcwd()
 
-player_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/playerStats.csv")
-advanced_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/AdvancedStats.csv")
+#player_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/playerStats.csv")
+#advanced_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/AdvancedStats.csv")
 attendance = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/attendance.csv")
-stats_and_salaries = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/stats_and_salary.csv")
-player_wikis = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/player_wikis.csv")
+sorted_by_points = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/stats_and_salary.csv")
+#player_wikis = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/player_wikis.csv")
+team_values = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/team_values.csv")
 
-teams = attendance.loc["0":, "TEAM"].values.tolist()
-att_percent = attendance.loc["0":, "PCT"].values.tolist()
+attendance.sort_values(by=["TEAM"], ascending=[True], inplace=True)
+team_values.sort_values(by=["TEAM"], ascending=[True], inplace=True)
+sorted_by_salary = sorted_by_points.sort_values(by=["SALARY_MILLIONS"], ascending=[False])
+
+teams = attendance["TEAM"]
+att_percent = attendance["PCT"]
+val = team_values["VALUE_MILLIONS"]
+
+total = 0
+for num in att_percent:
+        total += num
+avg_attendance = total/30
+
+fig = px.scatter(sorted_by_salary, x="SALARY_MILLIONS", y="RPM", hover_name="PLAYER", hover_data=["SALARY_MILLIONS","RPM","POINTS","TRB","AST"])
 
 app = dash.Dash(__name__)
 server = app.server
 app.title = "Dashboard"
 topMenu = html.Header(role='banner', children=[
         html.Div([
-                html.A("Home", href="/")
+                html.A("Home", href="/"),
+                html.A("Top 10 lists", href="top-10")
         ])
 ])
 
@@ -39,24 +53,34 @@ index_page = html.Div(children=[
         html.Br(),
         dcc.Graph(figure={
                 'data': [
-                        {'x': teams, 'y': att_percent, 'type': 'bar', 'name': 'name'},
+                        {'x': teams, 'y': att_percent, 'type': 'bar', 'name': 'name'}
                 ],
                 'layout': {
                         'title': 'Percentage of Attendance per team'
                 }
         }),
-
-        html.H2("Second Graph"),
-
-        
-
+        html.Br(),
+        html.H2("Which team is the most valuable?"),
+        dcc.Graph(figure={
+                'data': [
+                        {'x': teams, 'y': val, 'type':'bar', 'name': 'Team Value'}
+                ],
+                'layout': {
+                        'title': 'Value of Team (In Millions)'
+                }
+        }),
 ])
 
+top_10 = html.Div(children=[
+        html.H1("Top players"),
+        dcc.Graph(figure=fig)
+
+])
 @app.callback(Output(component_id="content", component_property="children"),
 [Input(component_id='url', component_property="pathname")])
 def display_page(pathname):
-        if pathname =="/page-1":
-                return page_1_layout
+        if pathname =="/top-10":
+                return top_10
         else:
                 return index_page
 
