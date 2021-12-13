b69e8c9daff58be206b63c0faee4b183e8590c86
josephc2195 <chicaj1221@gmail.com>
2021-12-12 21:00:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ cwd = os.getcwd()
 big_data = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/nba_2016_2017_100.csv")
 attendance = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/attendance.csv")
 player_stats = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/stats_and_salary.csv")
-#player_wikis = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/player_wikis.csv")
+player_wikis = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/player_wikis.csv")
 team_values = pd.read_csv("https://raw.githubusercontent.com/josephc2195/3190-ETL/master/Datasets/team_values.csv")
 
 attendance.sort_values(by=["TEAM"], ascending=[True], inplace=True)
@@ -32,6 +32,13 @@ for num in att_percent:
         total += num
 avg_attendance = total/30
 
+selected = player_wikis[["names", "pageviews"]]
+player_pageviews = selected.copy()
+new_df = player_pageviews.groupby(["names"]).sum()
+new_df.sort_values(by=["pageviews"], ascending=[False], inplace=True)
+top_50 = new_df.head(50)
+players_50 = top_50["names"]
+pageviews_50 = top_50["pageviews"]
 
 scatter_rpm_salary = px.scatter(player_stats, x="RPM", y="SALARY_MILLIONS", hover_name="PLAYER", hover_data=["SALARY_MILLIONS","RPM","POINTS","TRB","AST"])
 scatter_rpm_salary.update_layout(
@@ -39,6 +46,11 @@ scatter_rpm_salary.update_layout(
         yaxis_title="2016-2017 Salary"
 )
 
+scatter_followers_salary = px.scatter(player_stats, x="TWITTER_FOLLOWERS", y="SALARY_MILLIONS", hover_name="PLAYER", hover_data=["SALARY_MILLIONS", "TWITTER_FOLLOWERS"])
+scatter_followers_salary.update_layout(
+        xaxis_title="Twitter followers",
+        yaxis_title="2016-2017 Salary"
+)
 
 
 app = dash.Dash(__name__)
@@ -46,8 +58,8 @@ server = app.server
 app.title = "NBA Stats Dashboard"
 topMenu = html.Header(role='banner', children=[
         html.Div([
-                html.A("Home", href="/", style={"padding":10}),
-                html.A("Breakdown", href="/breakdown")
+                html.A("Tean stats", href="/", style={"padding":10}),
+                html.A("Player stats", href="/players_breakdown")
         ])
 ])
 
@@ -79,21 +91,34 @@ index_page = html.Div(children=[
         }),
 ])
 
-breakdown = html.Div(children=[
+players_page = html.Div(children=[
         html.H1("Top players"),
         html.H3("This graph shows the production of a player vs their salary"),
         dcc.Graph(figure=scatter_rpm_salary),
         html.H3("As we can see Lebron James was the most paid for the year, but also was by far the best player (according to Real Plus-Minus)"),
         html.Br(),
 
-        html.H3("Production on the court isn't the only measure of someones value to an NBA team. Lets see if popular players on social media have an affect on attendance which would be added revenue for a team.")
+        html.H3("There are other factors that go into account when deciding a players salary."),
+        html.H3("Does social media presense effect their compensation?"),
+        dcc.Graph(figure=scatter_followers_salary),
+        html.Br(),
+        html.H3("Which players are the most popular in terms of pageviews?"),
+        dcc.Graph(figure={
+                'data': [
+                        {'x': players_50, 'y': pageviews_50, 'type': 'bar', 'name': 'name'}
+                ],
+                'layout': {
+                        'title': 'Top 50 popular players'
+                }
+        }),
+
 
 ])
 @app.callback(Output(component_id="content", component_property="children"),
 [Input(component_id='url', component_property="pathname")])
 def display_page(pathname):
-        if pathname =="/breakdown":
-                return breakdown
+        if pathname =="/players_breakdown":
+                return players_page
         else:
                 return index_page
 
